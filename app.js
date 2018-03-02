58c6875b166c386d3dcc5d7921c88d19cf93fc8f
Eugenio <eugenio_8778@hotmail.com>
2018-03-02 13:12:11 +0100

    Fix de login y registro. Peticiones en general al backend

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ var app = express();
 // Cargamos middlewares de body-parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+});
 
 // cargamos las rutas
 var accounts_routes = require('./routes/accounts');
